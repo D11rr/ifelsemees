d6e45dc8212fff9f688228201c9bad53efb12ebe
D11rr <145238391+D11rr@users.noreply.github.com>
2024-05-16 09:43:15 +0300

    Add files via upload

---
+++ kui ja siis harjutus/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Headers;
+
+namespace kui_ja_siis_harjutus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int auto = 1000;
+            int rekka = 5000;
+
+            if (auto < rekka) 
+            {
+                int vahe = (rekka - auto); 
+                Console.WriteLine("auto on " + vahe + " kg kergem kui rekka");
+            }
+            else
+            {
+                Console.WriteLine("rekka on raskem kui auto");
+            }
+        }
+    }
+}
